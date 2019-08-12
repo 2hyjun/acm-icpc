825c3143c9aeb59ace068431a73273f4ead0fc24
2hyjun <biper94@gmail.com>
2019-08-12 22:13:48 +0900

    attemp 1

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ readline.createInterface(process.stdin, process.stdout)
 
 
 function main() {
-	console.log('good')
+	const length = 10;
+	const set = new Set([]);
+
+	input.forEach(i => {
+		set.add(i % 42);
+	});
+
+	console.log(set.size);
 }
 
 
