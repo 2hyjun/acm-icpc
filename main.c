6ec6b109565c7a6f36afb477c6e324fd1e8914da
2hyjun <biper94@gmail.com>
2019-08-12 19:56:53 +0900

    2558

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int l, r;
-  scanf("%d", &l);
-  scanf("%d", &r);
+    int l, r;
+    scanf("%d", &l);
+    scanf("%d", &r);
 
 
-  printf("%d\n", l * ( r % 10));
-  printf("%d\n", l * ((r % 100) / 10));
-  printf("%d\n", l * (r / 100));
+    printf("%d\n", l * ( r % 10));
+    printf("%d\n", l * ((r % 100) / 10));
+    printf("%d\n", l * (r / 100));
 
-  printf("%d", l * r);
-
-
+    printf("%d", l * r);
+    return 0;
 }
