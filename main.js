275abe6787abc71b672eb140cf2e60f103b9ff39
2hyjun <biper94@gmail.com>
2019-08-12 22:43:34 +0900

    attemp 1

--- main.js
+++ main.js
@@ -7,7 +7,22 @@ readline.createInterface(process.stdin, process.stdout)
 
 
 function main() {
-	console.log('good')
+	const T = parseInt(input.shift());
+
+	input.forEach(line => {
+		const arr = line.split(' ');
+		const size = arr.shift()
+		const sum = arr.reduce((p, c) => p + parseInt(c), 0);
+		const avg = sum / parseInt(size);
+
+
+		const good = arr.filter(i => i > avg).length;
+
+		console.log(`${((good / size) * 100).toFixed(3)}%`);
+
+
+
+	});
 }
 
 
