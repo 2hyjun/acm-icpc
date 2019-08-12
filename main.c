b62641e8c6ee61b5e6d1a2bf2479ce42c236e729
2hyjun <biper94@gmail.com>
2019-08-12 19:56:24 +0900

    2558

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int l, r;
+  scanf("%d", &l);
+  scanf("%d", &r);
+
+
+  printf("%d\n", l * ( r % 10));
+  printf("%d\n", l * ((r % 100) / 10));
+  printf("%d\n", l * (r / 100));
+
+  printf("%d", l * r);
+
+
+}
