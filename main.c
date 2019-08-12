35b750c7f260c5f1d56c36cde39b49f841be307c
2hyjun <biper94@gmail.com>
2019-08-12 21:11:36 +0900

    attemp 1

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	int cnt = 0;
+	int org;
+	int num;
+	scanf("%d", &org);
+
+	num = org;
+
+	while (1) {
+		cnt++;
+		int two = num / 10;
+		int one = num % 10;
+		int sum = one + two;
+
+		num = (sum % 10) + (num % 10) * 10;
+
+		if (num == org) break;
+	}
+    printf("%d", cnt);
     return 0;
 }
