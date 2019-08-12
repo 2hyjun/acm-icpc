866bd1bd6ac6831e3c33b569370c3d396c4ab7f4
2hyjun <biper94@gmail.com>
2019-08-12 22:29:03 +0900

    attemp 1

--- main.js
+++ main.js
@@ -7,7 +7,18 @@ readline.createInterface(process.stdin, process.stdout)
 
 
 function main() {
-	console.log('good')
+	const length = Number(input.shift());
+	const nums = input[0].split(' ').map(i => Number(i));
+	const max = Math.max(...nums);
+
+	const sum = nums.reduce((p, c) => {
+		return p + c / max * 100
+	}, 0);
+
+
+	console.log(sum / length);
+
+
 }
 
 
