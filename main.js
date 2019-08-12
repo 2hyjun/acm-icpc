7d8b574f879972228e9f7917bbdf1422d47f4efe
2hyjun <biper94@gmail.com>
2019-08-12 22:34:12 +0900

    attemp 1

--- main.js
+++ main.js
@@ -7,8 +7,27 @@ readline.createInterface(process.stdin, process.stdout)
 
 
 function main() {
-	console.log('good')
+	const T = parseInt(input.shift());
+	
+	for(let i = 0; i < T; i++) {
+		console.log(calc(input[i]));
+	}
 }
 
+
+function calc(str) {
+	let sum = 0;
+	let cnt = 0;
+
+	[...str].forEach(s => {
+		if (s === 'O') {
+			sum += ++cnt;
+		} else if (s === 'X') {
+			cnt = 0;
+		}
+	})
+
+	return sum;
+}
 
 
