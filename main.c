38e64dcb105c33ecb3829752294a46ab4353ffb2
2hyjun <biper94@gmail.com>
2019-08-12 20:41:09 +0900

    attemp 1

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	int T;
+
+	scanf("%d", &T);
+	int i = 0;
+	for (i  = 0; i < T; i++) {
+		int l, r;
+		scanf("%d %d", &l, &r);
+		printf("%d\n", l + r);
+	}
+
     return 0;
 }
