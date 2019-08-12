570a92725983c7b7b1be891f8912a8141cb5fd9b
2hyjun <biper94@gmail.com>
2019-08-12 22:02:13 +0900

    main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const input = [];
+
+readline.createInterface(process.stdin, process.stdout)
+	.on('line', line => input.push(line.trim()))
+	.on('close', main);
+
+
+function main() {
+	console.log('good')
+}
+
+
+
