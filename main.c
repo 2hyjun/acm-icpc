bbacdad9f1d8996bb07d0d885b9cf39d0440316e
2hyjun <biper94@gmail.com>
2019-08-12 21:23:33 +0900

    attemp 1

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	int size;
+	scanf("%d", &size);
+
+	int* arr = (int*)malloc(size * sizeof(int));
+
+	int i = 0;
+
+	for (i = 0; i < size; i++) {
+		scanf("%d", arr + i);
+	}
+
+	int min = arr[0], max = arr[0];
+	for (i = 0; i < size; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		} else if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+
+	printf("%d %d", min, max);
+
+
+
     return 0;
 }
