9d168f1448f6f8e18dabc8d759f970205f4dbd28
2hyjun <biper94@gmail.com>
2019-08-12 21:37:28 +0900

    attemp 1

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	int size = 8;
+
+	int* arr = (int*)malloc(sizeof(int) * size);
+
+	int i = 0;
+	for (i = 0; i< size; i++) {
+		scanf("%d", arr + i);
+	}
+
+	int diff = arr[1] - arr[0];
+	int mixed = 0;
+
+
+
+	for (i = 0; i < size - 1; i++) {
+		int newDiff = arr[i + 1] - arr[i];
+		if (diff != newDiff) {
+			mixed = 1;
+			break;
+		} else if (newDiff != 1 && newDiff != -1) {
+			mixed = 1;
+			break;
+		}
+
+	}
+
+	if (mixed) {
+		printf("mixed");
+	} else if (diff == 1) {
+		printf("ascending");
+	} else {
+		printf("descending");
+	}
+
+	
     return 0;
 }
