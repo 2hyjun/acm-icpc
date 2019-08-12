2fe74f9b5f952510e9e4c34eaa0c0bace643bc51
2hyjun <biper94@gmail.com>
2019-08-12 20:36:41 +0900

    attemp 1

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+	int hour, minutes;
+	scanf("%d %d", &hour, &minutes);
+
+	minutes -= 45;
+
+	if (minutes < 0) {
+		minutes += 60;
+		hour -= 1; 
+	}
+
+	if (hour < 0) {
+		hour += 24;
+	}
+
+	printf("%d %d", hour, minutes);
+
+
     return 0;
 }
