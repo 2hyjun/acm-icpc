98a3137abbc11d098f08f4032c565fddc6b240d5
2hyjun <biper94@gmail.com>
2019-09-18 23:14:23 +0800

    feat: 10872

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ readline.createInterface(process.stdin, process.stdout)
 
 
 function main() {
-	console.log('good')
+	let N = parseInt(input[0]);
+	let sum = 1;
+	while(N > 0) {
+		sum *= N--;
+	}
+
+	console.log(sum);
 }
 
 
