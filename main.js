ef42a9cf034f1a6ca3b25df966e3e68e0dc9feb8
2hyjun <biper94@gmail.com>
2019-09-19 02:54:06 +0800

    temp

--- main.js
+++ main.js
@@ -7,7 +7,16 @@ readline.createInterface(process.stdin, process.stdout)
 
 
 function main() {
-	console.log('good')
+	const N = parseInt(input[0], 10);
+	const one = []
+	const two = []
+	const three = []
+
+	for (let i = N; i > 0; i--) {
+		one.push(i);
+	}
+
+	console.log(one);
 }
 
 
