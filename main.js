3a7096c480652b4f272815f1af66c59602fb034c
2hyjun <biper94@gmail.com>
2020-05-08 16:14:47 +0900

    feat: programmers - 연습문제

--- main.js
+++ main.js
@@ -7,7 +7,29 @@ readline.createInterface(process.stdin, process.stdout)
 
 
 function main() {
-	console.log('good')
+	const arr = JSON.parse(input[0])
+	console.log(solution(arr))
+}
+
+
+function solution(v) {
+	// v = // [[1, 4], [3, 4], [3, 10]]
+	var rstX = 0;
+	var rstY = 0;
+	v.forEach((em) => {
+
+		const x = em[0]
+		const y = em[1]
+
+		if (!rstX && v.filter(e => e[0] == x).length === 1) {
+			rstX = x
+		}
+
+		if (!rstY && v.filter(e => e[1] == y).length === 1) {
+			rstY = y
+		}
+	})
+	return [rstX, rstY]
 }
 
 
